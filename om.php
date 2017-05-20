3d4cd7c774318e3f5736ef8efb7deec93a16394e
MDUKian <kianbaywatch@gmail.com>
2017-05-20 14:29:08 +0200

    Diverse

--- om.php
+++ om.php
@@ -13,18 +13,45 @@ include "includes/nav.php";
                       <a class="ctabtn" href="om.php">AARHUS</a>
                       <a class="ctabtn" href="om.php">AARHUS</a>          
                       </div>                   </header>
-                    <section>
-                        <h2>article section h2</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices. Proin in est sed erat facilisis pharetra.</p>
+                    <section class="landercontent">
+                        <div>
+                            <img src="./media/img/klatre_ikon.png" alt="">
+                            <p>Hos <?php echo $city ?>Boulders tilbyder vi funktionel træning i form af bouldering. Det er nemt at komme i gang - man behøver hverken udstyr eller klatreerfaring ob_get_cleanvores dygtige og engagerede instruktører og ansatte står altid klar til at hjælpe dig godt i gang. <br>Prøv det i dag!</p>
+<div class="btncontainer">
+    
+    <a class="ctabtn" href="#.php">bliv medlem</a> 
+</div>
+
+                        </div>
+                                                <div>
+                            <img src="./media/img/kaffe_ikon.png" alt="">
+                            <p>Grundværdierne hos <?php echo $city ?>Boulders er sundhed og økologi. Du finder derfor en økologisk bar med et stort udvalg af kaffe, the, frisk juice, frugt og delikatesser. Nyd din kaffe i vores store chillområde og få en hyggelig snak med de andre medlemmer. <br>Vores kaffe skal bare prøves!</p>
+<div class="btncontainer">
+    
+    <a class="ctabtn" href="okobar.php">øko bar</a> 
+</div>
+
+                        </div>
+                                                <div>
+                            <img src="./media/img/hjerte_ikon.png" alt="">
+                            <p>Hos <?php echo $city ?>Boulders går vi meget op i at skabe et tæt fællesskab mellem vores medlemmer. Derfor afholder vi den første fredag i hver måned Late Night Bouldering med lækker musik, sjov klatring i alle niveauer, hyggeligt samvær og økologiske øl. <br>Kom og deltag i hyggen!</p>
+<div class="btncontainer">
+    
+    <a class="ctabtn" href="kalender.php">kalender</a> 
+</div>
+
+                        </div>
+
                     </section>
                     <section>
-                        <h2>article section h2</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices. Proin in est sed erat facilisis pharetra.</p>
+                        <h2>følg os på instagram</h2>
+<a href="#">@aarhusboulders</a>
+<div class="instawidget">
+    <!-- LightWidget WIDGET FRA https://lightwidget.com/ -->
+    <script src="//lightwidget.com/widgets/lightwidget.js"></script><iframe src="//lightwidget.com/widgets/5d959a837c575a188ddbddbc4ab85a27.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
+</div>
                     </section>
-                    <footer>
-                        <h3>article footer h3</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor.</p>
-                    </footer>
+
                 </article>
 
 
