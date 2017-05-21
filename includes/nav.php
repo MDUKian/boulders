a34278425ae494fbc1b69aa4c393749696bacbe9
MDUKian <kianbaywatch@gmail.com>
2017-05-21 20:35:35 +0200

    Pre-menu

--- includes/nav.php
+++ includes/nav.php
@@ -8,12 +8,23 @@
 
         <div class="header-container">
             <header class="wrapper clearfix">
-                <nav> <!--Alternativ PHP syntax til active class fra http://php.net/manual/en/control-structures.alternative-syntax.php -->
+                <nav id="navlarge"> <!--Alternativ PHP syntax til active class fra http://php.net/manual/en/control-structures.alternative-syntax.php -->
                     <ul>
                         <li><a <?php if($this_page == "om.php"):?>class="active"<?php endif;?> href="om.php"><? echo $city ?> Boulders</a></li>
                         <li><a <?php if($this_page == "praktiskinfo.php"):?>class="active"<?php endif;?> href="praktiskinfo.php">praktisk info</a></li>
                         <li><a <?php if($this_page == "okobar.php"):?>class="active"<?php endif;?> href="okobar.php">øko bar</a></li>
-                        <li><a  href="index.php"><img id="toplogo" src="./media/img/logohalf.png"></a></li>
+                        <li><a  href="index.php"><img id="logolarge" src="./media/img/logohalf.png"></a></li>
+                        <li><a <?php if($this_page == "medlemskab.php"):?>class="active"<?php endif;?> href="medlemskab.php">pris &amp; medlemskab</a></li>
+                        <li><a <?php if($this_page == "kalender.php"):?>class="active"<?php endif;?> href="kalender.php">kalender</a></li>
+                        <li><a <?php if($this_page == "kontakt.php"):?>class="active"<?php endif;?> href="kontakt.php">kontakt</a></li>
+                    </ul>
+                </nav>
+                <nav id="navsmall"> <!--Alternativ PHP syntax til active class fra http://php.net/manual/en/control-structures.alternative-syntax.php -->
+                    <ul>
+                        <li><a <?php if($this_page == "om.php"):?>class="active"<?php endif;?> href="om.php"><? echo $city ?> Boulders</a></li>
+                        <li><a <?php if($this_page == "praktiskinfo.php"):?>class="active"<?php endif;?> href="praktiskinfo.php">praktisk info</a></li>
+                        <li><a <?php if($this_page == "okobar.php"):?>class="active"<?php endif;?> href="okobar.php">øko bar</a></li>
+                        <li><a  href="index.php"><img id="logolarge" src="./media/img/logohalf.png"></a></li>
                         <li><a <?php if($this_page == "medlemskab.php"):?>class="active"<?php endif;?> href="medlemskab.php">pris &amp; medlemskab</a></li>
                         <li><a <?php if($this_page == "kalender.php"):?>class="active"<?php endif;?> href="kalender.php">kalender</a></li>
                         <li><a <?php if($this_page == "kontakt.php"):?>class="active"<?php endif;?> href="kontakt.php">kontakt</a></li>
